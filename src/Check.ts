6fdfc2e3fa19f4c7ed8c709e34886358080c1b3a
Anastasiya Borodina <>
2022-02-23 15:37:08 +0400

    change names, add data initializing in Check

--- src/Check.ts
+++ src/Check.ts
@@ -1,10 +1,10 @@
-import { OrderPosition } from "./OrderPosition";
-import { Payment } from "./Payment";
+import { OrderPositionI } from "./OrderPosition";
+import { PaymentI } from "./Payment";
 
 /**
  * Система налогообложения
  */
-export enum TaxSystem {
+export enum TaxSystemT {
 	common = 0, // Общая
 	simplifiedIn = 1, // Упрощённая (доходы)
 	simplifiedInOut = 2, // Упрощённая (доходы минут расходы)
@@ -15,7 +15,7 @@ export enum TaxSystem {
 /**
  * Вид чека
  */
-export enum CheckIntent {
+export enum CheckIntentT {
     sell = 'sell', //  приход (используется при продаже товара или услуги)
     sellReturn = 'sellReturn', // возврат прихода
     sellCorrection = 'sellCorrection', // коррекция прихода
@@ -24,38 +24,54 @@ export enum CheckIntent {
     buyCorrection = 'buyCorrection', // коррекция расхода
 }
 
-export class Check {
-    public intent: CheckIntent; // Направление платежа
-	public external_id: number; // Уникальный идентификатор, который назначается самим магазином
-	public sno: TaxSystem; // Система налогообложения
-	public user: string; // E-mail, на который отправится чек
-	public print = true; // нужно ли печатать
-	public positions: OrderPosition[] = []; // Товары
-	public payments: Payment[] = []; // Платежи
-	public cashier: string; // ФИО кассира
-	public payment_address: string; // Место платежа - URL сайта
+/**
+ * Чек
+ */
+export interface CheckI {
+    intent?: CheckIntentT,
+    external_id: number,
+    sno: TaxSystemT,
+    user: string,
+    print: boolean,
+    positions?: OrderPositionI[],
+    payments?: PaymentI[],
+    cashier: string,
+    payment_address: string,
+}
 
-    constructor (
-        intent: CheckIntent,
-        idExternal: number,
-        sno: TaxSystem,
-        userContact: string,
-        paymentAddress: string,
-        cashier: string,
-    ) {
-        this.intent = intent;
-        this.external_id = idExternal;
-        this.sno = sno;
-        this.user = userContact;
-        this.payment_address = paymentAddress;
-        this.cashier = cashier;
+export class Check {
+    public intent: CheckIntentT = CheckIntentT.sell; // Направление платежа
+	public idExternal: number; // Уникальный идентификатор, который назначается самим магазином
+	public sno: TaxSystemT; // Система налогообложения
+	public userContact: string; // E-mail, на который отправится чек
+	public print = true; // нужно ли печатать
+	public positions: OrderPositionI[] = []; // Товары
+	public payments: PaymentI[] = []; // Платежи
+	public cashier: string; // ФИО кассира
+	public paymentAddress: string; // Место платежа - URL сайта
+
+    constructor (data: CheckI) {
+        if (data.intent) {
+            this.intent = data.intent;
+        }
+
+        this.idExternal = data.external_id;
+        this.sno = data.sno;
+        this.userContact = data.user;
+
+        if (typeof data.print === 'boolean') {
+            this.print = data.print;
+        }
+
+        this.cashier = data.cashier;
+        this.paymentAddress = data.payment_address;
     }
 
-    public addPosition(orderPosition: OrderPosition) {
+    public addPosition(orderPosition: OrderPositionI) {
         this.positions.push(orderPosition)
     }
 
-    public addPayment(payment: Payment) {
+    public addPayment(payment: PaymentI) {
         this.payments.push(payment)
     }
 }
